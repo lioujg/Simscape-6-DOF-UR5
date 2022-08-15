2e94bac082f3a6beb21bb1dcb86e44bdb6ec16cf
Liu <76809218+lioujg@users.noreply.github.com>
2022-08-15 21:31:01 +0800

    X direction Impedance control

--- Robots/universalUR5/start_UR5.m
+++ Robots/universalUR5/start_UR5.m
@@ -9,3 +9,10 @@ addpath([pwd filesep 'Geometry']);
 robot = importrobot('sm_universalUR5.urdf');
 % open('sm_universalUR5_1_RawImport.slx');
 open('pp.slx');
+
+%% Parameter of Joint
+bound = 1e-6;
+stiff = 1000;
+damping = 10;
+tran = 1e-6;
+degree = 0.5;
