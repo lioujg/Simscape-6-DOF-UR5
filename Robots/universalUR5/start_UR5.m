846a280e49031ecf64bd671d1448ed4bf6f69cef
Liu <76809218+lioujg@users.noreply.github.com>
2022-08-26 15:45:18 +0800

    signal editor

--- Robots/universalUR5/start_UR5.m
+++ Robots/universalUR5/start_UR5.m
@@ -9,13 +9,4 @@ addpath([pwd filesep 'Geometry']);
 robot = importrobot('sm_universalUR5.urdf');
 % open('sm_universalUR5_1_RawImport.slx');
 open('pp.slx');
-
-%% Parameter of Joint
-bound = 1e-6;
-stiff = 1000;
-damping = 10;
-tran = 1e-6;
-degree = 0.5;
-
-%% Measurement Point
-rp = [-0.2 0 0.1];
+parameter_setting;
