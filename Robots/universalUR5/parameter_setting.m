a5553273461a432c4b574564475072d7d799c54e
Liu <76809218+lioujg@users.noreply.github.com>
2022-08-29 22:11:31 +0800

    adjusting inertia gain

--- Robots/universalUR5/parameter_setting.m
+++ Robots/universalUR5/parameter_setting.m
@@ -5,40 +5,44 @@ damping = 10;
 tran = 1e-6;
 degree = 0.5;
 
+%% Gimbal Joint
+stiff_g = 0.3;
+damp_g = 0.05;
+
 %% Measurement Point
-rp = [-0.2 0 0.1];
+rp = [-0.1 0 0.1];
 
 %% Ref Signal
 % translation
 % x
-vx_amp = -0.1;
-vx_f = 0.4;
+vx_amp = -0.2;
+vx_f = 0.45;
 vx_start = 0;
 vx_end = 50;
 % y
-vy_amp = -0.1;
+vy_amp = -0.25;
 vy_f = 0.25;
 vy_start = 0;
 vy_end = 50;
 % z
-vz_amp = 0.00;
-vz_f = 0.1;
+vz_amp = -0.1;
+vz_f = 0.35;
 vz_start = 0;
 vz_end = 50;
 
 % rotation
 % x
 wx_amp = 0.4;
-wx_f = 0.1;
+wx_f = 0.55;
 wx_start = 0;
 wx_end = 50;
 % y
-wy_amp = 0.2;
-wy_f = 0.35;
+wy_amp = 0.25;
+wy_f = 0.45;
 wy_start = 0;
 wy_end = 50;
 % z
-wz_amp = 0.15;
-wz_f = 0.15;
+wz_amp = 0.35;
+wz_f = 0.4;
 wz_start = 0;
 wz_end = 50;
