82ecb97ee0cadce2a8b96ec425419e09926db03c
Liu <76809218+lioujg@users.noreply.github.com>
2022-08-30 17:03:24 +0800

    finished inertia gain and trajectory

--- Robots/universalUR5/parameter_setting.m
+++ Robots/universalUR5/parameter_setting.m
@@ -15,34 +15,34 @@ rp = [-0.1 0 0.1];
 %% Ref Signal
 % translation
 % x
-vx_amp = -0.2;
+vx_amp = 0.15;
 vx_f = 0.45;
 vx_start = 0;
 vx_end = 50;
 % y
 vy_amp = -0.25;
-vy_f = 0.25;
+vy_f = 0.35;
 vy_start = 0;
 vy_end = 50;
 % z
-vz_amp = -0.1;
+vz_amp = -0.25;
 vz_f = 0.35;
 vz_start = 0;
 vz_end = 50;
 
 % rotation
 % x
-wx_amp = 0.4;
+wx_amp = 0.7;
 wx_f = 0.55;
 wx_start = 0;
 wx_end = 50;
 % y
-wy_amp = 0.25;
-wy_f = 0.45;
+wy_amp = 0.7;
+wy_f = 0.75;
 wy_start = 0;
 wy_end = 50;
 % z
-wz_amp = 0.35;
-wz_f = 0.4;
+wz_amp = 1.2;
+wz_f = 0.7;
 wz_start = 0;
 wz_end = 50;
